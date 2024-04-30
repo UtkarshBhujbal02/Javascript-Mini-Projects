814d270c477b772cd5f9fe8cf0ff0f8f79f5a42f
Utkarsh <utkarshbhujbal02@gmail.com>
2024-04-30 19:58:31 +0530

    30/4/24

--- ToDoListApp/index.js
+++ ToDoListApp/index.js
@@ -4,11 +4,36 @@ const todoList = document.getElementById("todoList")
 
 
 const addTodo = ()=>{
-    const inputText = inputBox.ariaValueMax.trim();
+    const inputText = inputBox.value.trim();
     if(inputText.length <= 0){
         alert("You must write something in your to do");
+        return false;
 
     }
+
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerHTML = inputText;
+    li.appendChild(p);
+    inputBox.value = "";
+
+    const editBtn = document.createElement("button")
+    editBtn.innerText = "Edit";
+    editBtn.classList.add("btn" , "editBtn");
+    li.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.innerText = "Remove";
+    deleteBtn.classList.add("btn" , "deleteBtn");
+    li.appendChild(deleteBtn);
+
+
+    todoList.appendChild(li);
+    
+    
+    
+    
+
 }
 addBtn.addEventListener('click',addTodo);
 
